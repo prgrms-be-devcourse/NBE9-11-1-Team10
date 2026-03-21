61ef86172e50063dee28da44f2926974f7b8d1d7
jowoojin <codewrite123@gmail.com>
2026-03-21 16:13:26 +0900

    feat : 재고 조회 api 구현

---
+++ backend/src/main/java/com/gridsandcircles/gc_coffee/stock/controller/StockController.java
@@ -0,0 +1,34 @@
+package com.gridsandcircles.gc_coffee.stock.controller;
+
+
+import com.gridsandcircles.gc_coffee.entity.Product;
+import com.gridsandcircles.gc_coffee.global.dto.ApiResponse;
+import com.gridsandcircles.gc_coffee.stock.repository.ProductStockRepository;
+import com.gridsandcircles.gc_coffee.stock.service.StockService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class StockController {
+
+    private final StockService stockService;
+
+    private final ProductStockRepository productStockRepository;
+    //api/v1/product/{productId}/stock?requsetQuantity=재고수량
+    @GetMapping("api/v1/product/{productId}/stock")
+    public ResponseEntity<ApiResponse<Void>> checkStock(
+            @PathVariable("productId") Long productId,
+            @RequestParam(required = false,defaultValue = "0") int requestQuantity
+    ) {
+
+        //고객이 +로 장바구니에 추가된 상품의 "번호"와 상품의 "재고 수량"이 필요하다.
+        stockService.validateStockAvailability(productId,requestQuantity);
+
+        return ResponseEntity.ok(ApiResponse.ok());
+    }
+
+}

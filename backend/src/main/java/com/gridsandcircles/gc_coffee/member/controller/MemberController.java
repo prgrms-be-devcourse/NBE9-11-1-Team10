c1b908a4d23ea69479015c8d5c7b3b439f4d6456
sat7312 <sat7312@gmail.com>
2026-03-24 10:14:37 +0900

    refactor: MemberController ApiREsponse 응답 통일

--- backend/src/main/java/com/gridsandcircles/gc_coffee/member/controller/MemberController.java
+++ backend/src/main/java/com/gridsandcircles/gc_coffee/member/controller/MemberController.java
@@ -4,8 +4,8 @@ import com.gridsandcircles.gc_coffee.global.dto.ApiResponse;
 import com.gridsandcircles.gc_coffee.member.dto.MemberRequest;
 import com.gridsandcircles.gc_coffee.member.dto.MemberResponse;
 import com.gridsandcircles.gc_coffee.member.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,11 +19,9 @@ public class MemberController {
     private final MemberService memberservice;
 
     @PostMapping
-    public ResponseEntity<ApiResponse<MemberResponse>> createOrFindMember(
-            @RequestBody MemberRequest request){
+    public ApiResponse<MemberResponse> createOrFindMember(
+            @RequestBody @Valid MemberRequest request){
         MemberResponse response = memberservice.findOrCreateMember(request);
-
-        return ResponseEntity.ok(ApiResponse.ok(response));
+        return ApiResponse.ok(response);
     }
-
 }

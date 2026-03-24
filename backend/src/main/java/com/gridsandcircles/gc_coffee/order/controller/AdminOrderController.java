2ac3e687ded043ddaa9ce91a598886a1d3a50069
sat7312 <sat7312@gmail.com>
2026-03-24 12:23:03 +0900

    fix : 상품 등록 응답에 가격/재고 응답 추가 (#77)

--- backend/src/main/java/com/gridsandcircles/gc_coffee/order/controller/AdminOrderController.java
+++ backend/src/main/java/com/gridsandcircles/gc_coffee/order/controller/AdminOrderController.java
@@ -1,15 +1,9 @@
 package com.gridsandcircles.gc_coffee.order.controller;
 
-import com.gridsandcircles.gc_coffee.entity.Order;
-import com.gridsandcircles.gc_coffee.global.dto.ApiResponse;
-import com.gridsandcircles.gc_coffee.order.dto.OrderListResponse;
 import com.gridsandcircles.gc_coffee.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,14 +12,14 @@ public class AdminOrderController {
 
     private final OrderService orderService;
 
-    @GetMapping
-    public ApiResponse<List<OrderListResponse>> list(){
-        List<Order> orderList = orderService.list();
-
-        List<OrderListResponse> orderListResponseList = orderList.reversed().stream()
-                .map(OrderListResponse::new)
-                .toList();
-
-        return ApiResponse.ok(orderListResponseList);
-    }
+//    @GetMapping
+//    public ApiResponse<List<OrderListResponse>> list(){
+//        List<Order> orderList = orderService.list();
+//
+//        List<OrderListResponse> orderListResponseList = orderList.reversed().stream()
+//                .map(OrderListResponse::new)
+//                .toList();
+//
+//        return ApiResponse.ok(orderListResponseList);
+//    }
 }
--- backend/src/main/java/com/gridsandcircles/gc_coffee/product/dto/ProductCreateResponse.java
+++ backend/src/main/java/com/gridsandcircles/gc_coffee/product/dto/ProductCreateResponse.java
@@ -2,8 +2,8 @@ package com.gridsandcircles.gc_coffee.product.dto;
 
 import com.gridsandcircles.gc_coffee.entity.Product;
 
-public record ProductCreateResponse(Long productId, String name){
-    public static com.gridsandcircles.gc_coffee.product.dto.ProductCreateResponse from(Product product){
-        return new com.gridsandcircles.gc_coffee.product.dto.ProductCreateResponse(product.getId(), product.getName());
+public record ProductCreateResponse(Long productId, String name, int price, int stock){
+    public static ProductCreateResponse from(Product product){
+        return new ProductCreateResponse(product.getId(), product.getName(), product.getPrice(), product.getStock());
     }
 }
